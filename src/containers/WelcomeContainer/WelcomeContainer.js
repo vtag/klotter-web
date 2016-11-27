aa169540f01232d77aed2d9ee0aea3b0263c5f1a
Thelin90 <simon.thelin@live.se>
2016-11-27 10:36:06 +0100

    Add image logo in WelcomeContainer

--- src/containers/WelcomeContainer/WelcomeContainer.js
+++ src/containers/WelcomeContainer/WelcomeContainer.js
@@ -3,9 +3,11 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import classNames from 'classnames'
+import logo from './klotter3.png'
 
 /* Internal Dependencies */
 import styles from './WelcomeContainer.scss'
+
 import { GeolocationActions } from '../../actions'
 
 class WelcomeContainer extends React.Component {
@@ -39,7 +41,7 @@ class WelcomeContainer extends React.Component {
         <div className={styles.container}>
           <div className={styles.logoContainer}>
             <p className={styles.logo}>
-              V tag
+              <img src={logo} />
             </p>
             <p className={styles.subtitle}>
               <i className="fa fa-dot-circle-o" aria-hidden="true"/>
