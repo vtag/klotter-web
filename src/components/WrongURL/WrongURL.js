1dc788b16b6631133b3381aee4779cc289580db9
Engine Seunghyun Park <engine@zoyi.co>
2016-11-27 17:38:21 +0900

    Modify routes and add WrongURL component

---
+++ src/components/WrongURL/WrongURL.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+/**/
+import styles from './WrongURL.scss'
+
+const WrongURL = (props) => {
+  return (
+    <div className={styles.wrapper}>
+      wrong url
+    </div>
+  )
+}
+
+export default WrongURL
